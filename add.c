1b07bd0e40507b0db5c3a1be1cf2662579e17dbf
Alfred Tuva <muindetuva@gmail.com>
2021-05-13 14:36:50 +0000

    Remove word input

--- add.c
+++ add.c
@@ -12,7 +12,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (temp == NULL || temp->next == NULL)
 	{
-		fprintf(stderr, "L%d: can't add input, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	sum = (temp->n) + (temp->next->n);
