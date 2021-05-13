d32c855afca397ba475cf29fe58743e046d3e474
Alfred Tuva <muindetuva@gmail.com>
2021-05-13 16:13:27 +0000

    Add new line to error message

--- pop.c
+++ pop.c
@@ -14,7 +14,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		free_stack(*stack);
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
