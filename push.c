db4724a379d56542a2a90db87baf2ec19bd29441
Alfred Tuva <muindetuva@gmail.com>
2021-05-13 16:15:23 +0000

    Add new line to error message

--- push.c
+++ push.c
@@ -17,6 +17,7 @@ void push(stack_t **stack, unsigned int line_number)
 		{
 			fprintf(stderr, "L%d: usage: push integer\n",
 					line_number);
+			free_stack(*stack);
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -25,7 +26,7 @@ void push(stack_t **stack, unsigned int line_number)
 
 	if (new_node == NULL)
 	{
-		fprintf(stderr, "Error: malloc failed");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 	new_node->n = atoi(push_data);
