f112a5a37d07922c2d95be7e31b9df077b82760e
Koderua <kibirtbayou@yahoo.com>
2021-05-13 09:37:11 +0000

    comments

--- monty.h
+++ monty.h
@@ -56,7 +56,4 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
 
-
-
-
 #endif /* MONTY_H */
